6b1329a53419bcfc3ef09eaf12d3f33935d8d5b0
Roman <z_roman_05@mail.ru>
2025-04-25 17:07:25 +0300

    Initial commit

---
+++ algorithms_func_easy.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include "algos_header.h"
+using namespace std;
+
+
+//Задача 1: Подается три элемента, вывести средний, "центральный" элемент
+int SredniyElement(int a, int b, int c) {
+    
+	int arg[3] = { a,b,c };
+    int temp;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3 - i - 1; j++) {
+            if (arg[j] > arg[j + 1]) {
+                temp = arg[j];
+                arg[j] = arg[j + 1];
+                arg[j + 1] = temp;
+            }
+        }
+    }
+	return arg[1];
+}
+//Задача 5321: Подается К число уровней елки, на каждом уровне 2^(i-1) шариков, определить сколько шариков требуется для k уровней
+int NaryajElka(int k) {
+    int summa = 0;
+    for (int i = 1; i <= k; i++) {
+        summa += int(pow(2, i - 1));
+    }
+    return summa;
+}
+
+
+//Задача 55: Свинки стреляют по птичкам из ружья. У птичек есть координаты
+//Если попадают в птичку сверху, то она падает на всех птичек под ней.
+//Требуется найти минимальное количество выстрелов для снова всех птичек количесвом N
+int AngryPigs(int N)
+{
+    int x, y;
+    cin >> N;
+    set<int> counts;
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y;
+        counts.insert(x);
+    }
+
+    return counts.size();
+}
+
+//Задача 83: На клавиатуре есть N клавиш с количеством возможных нажатий на каждую клавишу,
+// Задается последовательность из K нажатий. Определить, работает ли клавиша после этой последовательности?
+int Keybord()
+{
+    int N, K;
+    cin >> N;
+    vector<int> pressing;
+    vector<int> sequence;
+    int cur;
+
+    for (int i = 0; i < N; i++) {
+        cin >> cur;
+        pressing.push_back(cur);
+    }
+
+    cin >> K;
+    for (int j = 0; j < K; j++) {
+        cin >> cur;
+        sequence.push_back(cur);
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (pressing[i] >= K) {
+            cout << "NO" << endl;
+            continue;
+        }
+        if (pressing[i] < count(sequence.begin(), sequence.end(), i + 1)) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
+
+//Задача 62: Количество различных чисел в списке
+int Different_numbers()
+{
+    set<int> massive;
+    int num;
+    while (cin >> num) {
+        massive.insert(num);
+        if (cin.peek() == '\n') {
+            break;
+        }
+    }
+    return massive.size();
+}
---
+++ algos_main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "algos_header.h"
+
+using namespace std;
+
+int main()
+{
+    //cout << "The middle element in the list: " << SredniyElement(1, 6, 3) << endl;
+    
+    //cout << "Count of cristmas toys on the tree: " << NaryajElka(3) << endl;
+    
+    //cout << "Min. Counts of shots to kill all flying birds: " << AngryPigs(3) << endl;
+
+    //cout << "The key [i] doesn't work? " << Keybord() <<endl;
+   
+    //cout << "Count of different numbers in the list: " << Different_numbers() << endl;
+
+    return 0;
+}
+
+
