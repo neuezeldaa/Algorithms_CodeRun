e96355aa19221b0f55afece5817f7483a5a650b9
Roman <z_roman_05@mail.ru>
2025-05-03 13:25:24 +0300

    Добавлена задача 99

--- algorithms_func_easy.cpp
+++ algorithms_func_easy.cpp
@@ -309,3 +309,59 @@ int intersecc()
     }
     return 0;
 }
+
+
+
+//Задача 99: Выяснить, есть ли i-элемент из второго множества в первом множестве с помощью бинарного поиска
+int binary_search(vector<int> vec, int i)
+{
+    int low = 0;
+    int high = vec.size() - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int guess = vec[mid];
+        if (guess == i) {
+            return 1;
+        }
+        if (guess > i) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+
+    }
+    return 0;
+}
+
+int check_vectors()
+{
+    vector<int> N_vec;
+    vector<int> K_vec;
+    int N, K;
+    cin >> N >> K;
+    int num;
+    for (int i = 0; i < (N + K); i++) {
+        cin >> num;
+        if (i < N) {
+            N_vec.push_back(num);
+        }
+        else {
+            K_vec.push_back(num);
+        }
+    }
+    sort(N_vec.begin(), N_vec.end());
+
+
+
+    for (int i : K_vec) {
+        if (binary_search(N_vec, i) != 0) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
--- algos_main.cpp
+++ algos_main.cpp
@@ -7,27 +7,29 @@ using namespace std;
 
 int main()
 {
-    //cout << "The middle element in the list: " << SredniyElement(1, 6, 3) << endl;
+    // 1 cout << "The middle element in the list: " << SredniyElement(1, 6, 3) << endl;
     
-    //cout << "Count of cristmas toys on the tree: " << NaryajElka(3) << endl;
+    // 2 cout << "Count of cristmas toys on the tree: " << NaryajElka(3) << endl;
     
-    //cout << "Min. Counts of shots to kill all flying birds: " << AngryPigs(3) << endl;
+    // 3 cout << "Min. Counts of shots to kill all flying birds: " << AngryPigs(3) << endl;
 
-    //cout << "The key [i] doesn't work? " << Keybord() <<endl;
+    // 4 cout << "The key [i] doesn't work? " << Keybord() <<endl;
    
-    //cout << "Count of different numbers in the list: " << Different_numbers() << endl;
+    // 5 cout << "Count of different numbers in the list: " << Different_numbers() << endl;
    
-    //cout << "Exercise about poliglotes" << poliglott() << endl;
+    // 6 cout << "Exercise about poliglotes" << poliglott() << endl;
 
-    //cout << "Colors of kubiki:" << kubiki(3, 4) << endl;
+    // 7 cout << "Colors of kubiki:" << kubiki(3, 4) << endl;
 
-    //cout << "Count of added numbers: " << OpenCalculator(1, 2, 3, 456789) << endl;
+    // 8 cout << "Count of added numbers: " << OpenCalculator(1, 2, 3, 456789) << endl;
 
-    //cout << "Maximum of profit: " << AlotOfChairs(2, 3) << endl;
+    // 9 cout << "Maximum of profit: " << AlotOfChairs(2, 3) << endl;
 
-    //cout << "Probability of error on each server: " << Error(2) << endl;
+    // 10 cout << "Probability of error on each server: " << Error(2) << endl;
 
-    //cout << "Intersection of two vectors: " << intersecc() << endl;
+    // 11 cout << "Intersection of two vectors: " << intersecc() << endl;
+    
+    // 12 cout << "Realisation of binary search function: " << check_vectors() << endl;
 
 
     return 0;
